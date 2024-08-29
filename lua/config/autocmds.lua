2412f78f5a916b7609699514efb0e71498e5d333
Jari Zwarts <j.zwarts@atabix.nl>
2024-08-29 13:34:16 +0200

    autoremove unused imports

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,8 +13,30 @@
 -- })
 --
 vim.api.nvim_create_autocmd("DirChanged", {
-    pattern = "*",
-    callback = function()
-        vim.cmd("Neotree reveal")
-    end,
+  pattern = "*",
+  callback = function()
+    -- require("persistence").load()
+    vim.cmd("Neotree reveal")
+  end,
 })
+
+-- Function to run the LSP action
+local function remove_unused_imports()
+  -- Check if the current file is a TypeScript file
+  if vim.bo.filetype == "typescript" or vim.bo.filetype == "typescriptreact" then
+    -- Execute the LSP action for removing unused imports
+    -- vim.cmd('lua require("lazyvim.lsp").action["source.removeUnused.ts"]()')
+    LazyVim.lsp.action["source.removeUnused.ts"]()
+  end
+end
+
+-- Autocommand to trigger the function on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = remove_unused_imports,
+})
+-- vim.api.nvim_create_autocmd({ "DirChanged" }, {
+--   callback = function()
+--     -- Attempt to load the session for the new directory
+--     require("persistence").load()
+--   end,
+-- })
